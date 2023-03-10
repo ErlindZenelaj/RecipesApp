5f64f708949377cf6cc3ca48072a1a3f181c9b6e
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2023-03-10 18:13:43 +0100

    Login&Register with Firebase
    Co-Authored-By: Elmedina Morina <95186204+ElmedinaMorina@users.noreply.github.com>
    Co-Authored-By: DrilonHh <73119922+DrilonHh@users.noreply.github.com>
    Co-Authored-By: R1nor1 <96205312+R1nor1@users.noreply.github.com>

--- recipesapp/src/main.js
+++ recipesapp/src/main.js
@@ -4,8 +4,9 @@ import store from "./store";
 import "./style.css";
 import App from "./App.vue";
 import * as filters from "./filters";
+import stores from "./stores";
 
 const app = createApp(App);
-app.use(router).use(store).mount("#app");
+app.use(router).use(store).use(stores).mount("#app");
 
 app.config.globalProperties.$filters = filters;
--- recipesapp/src/router/index.js
+++ recipesapp/src/router/index.js
@@ -7,6 +7,8 @@ import MealsByLetter from "../views/MealsByLetter.vue";
 import MealsByName from "../views/MealsByName.vue";
 import MealDetails from "../views/MealDetails.vue";
 import Ingredients from "../views/Ingredients.vue";
+import { auth } from '../firebase'
+
 
 const routes = [
   {
@@ -17,31 +19,54 @@ const routes = [
         path: "/",
         name: "home",
         component: Home,
+        meta: {
+          requiresAuth: true
+        }
       },
       {
         path: "/by-name/:name?",
         name: "byName",
         component: MealsByName,
+        meta: {
+          requiresAuth: true
+        }
       },
       {
         path: "/by-letter/:letter?",
         name: "byLetter",
         component: MealsByLetter,
+        meta: {
+          requiresAuth: true
+        }
       },
       {
         path: "/ingredients",
         name: "ingredients",
         component: Ingredients,
+        meta: {
+          requiresAuth: true
+        }
       },
       {
         path: "/by-ingredient/:ingredient",
         name: "byIngredient",
         component: MealsByIngredient,
+        meta: {
+          requiresAuth: true
+        }
       },
       {
         path: "/meal/:id",
         name: "mealDetails",
         component: MealDetails,
+        meta: {
+          requiresAuth: true
+        }
+      },
+      {
+        path: "/login",
+        name: "Login",
+        component: () => import('../views/Login.vue')
       },
     ],
   },
@@ -55,5 +80,20 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login' && auth.currentUser) {
+    next('/')
+    return;
+  }
+
+  if (to.matched.some(record => record.meta.requiresAuth) && !auth.currentUser) {
+    next('/login')
+    return;
+  }
+
+  next();
+})
+
 
 export default router;
